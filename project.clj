b2ddf2f6bb88bca16b403607e6105043b387bd59
Stefan Hübner <sthuebner@googlemail.com>
2012-05-21 22:54:03 +0200

    the less dependencies the better

--- project.clj
+++ project.clj
@@ -3,10 +3,7 @@
   :test-directory "src"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [midje "1.3.1"]
-                 [cascalog "1.8.6"]]
+                 [cascalog "1.8.7"]]
   :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2"]
                      [midje-cascalog "0.4.0"]
-                     [org.slf4j/slf4j-simple "1.6.4"]
-                     [org.slf4j/jcl-over-slf4j "1.6.4"]
-                     [commons-codec "1.6"]
-                     [commons-httpclient "3.1"]])
+                     [org.slf4j/slf4j-simple "1.6.4"]])
