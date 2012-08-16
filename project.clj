53c4ff9fc1587e5cc06f926f5f37e1d302503d79
Stefan Hübner <stefan.hubner@nokia.com>
2012-08-16 10:50:12 +0200

    migrate project.clj to leiningen 2

--- project.clj
+++ project.clj
@@ -1,12 +1,15 @@
 (defproject cascalog-examples "1-SNAPSHOT"
   :description "A playground for Cascalog."
-  :test-directory "src"
+  :min-lein-version "2.0.0"
+  :test-paths ["src"]
+  :repositories {"conjars" "http://conjars.org/repo/"}
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [midje "1.3.1"]
-                 [cascalog "1.8.8-SNAPSHOT"]]
-  :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2"]
-                     [midje-cascalog "0.4.0"]
-                     [org.slf4j/slf4j-simple "1.6.4"]
-                     [org.slf4j/jcl-over-slf4j "1.6.4"]
-                     [commons-codec "1.6"]
-                     [commons-httpclient "3.1"]])
+                 [cascalog "1.10.0"]
+                 ;;[cascading/cascading-core "1.2.6"]
+                 ]
+  :profiles {:dev {:dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
+                                  [midje-cascalog "0.4.0"]
+                                  ;;[org.slf4j/slf4j-simple "1.4.3"]
+                                  ;;[org.slf4j/jcl-over-slf4j "1.4.3"]
+                                  ]}})
