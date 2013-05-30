957803ccd2439b664fc3b4942ed07d9531e00047
Stefan Hübner <sthuebner@googlemail.com>
2013-05-30 12:55:50 +0200

    changes in project.clj

--- project.clj
+++ project.clj
@@ -3,13 +3,8 @@
   :min-lein-version "2.0.0"
   :test-paths ["src"]
   :repositories {"conjars" "http://conjars.org/repo/"}
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [midje "1.3.1"]
-                 [cascalog "1.10.0"]
-                 ;;[cascading/cascading-core "1.2.6"]
-                 ]
+  :dependencies [[midje "1.3.1"]
+                 [cascalog "1.10.0"]]
   :profiles {:dev {:dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
                                   [midje-cascalog "0.4.0"]
-                                  ;;[org.slf4j/slf4j-simple "1.4.3"]
-                                  ;;[org.slf4j/jcl-over-slf4j "1.4.3"]
-                                  ]}})
+                                  [ritz/ritz-swank "0.5.0"]]}})
